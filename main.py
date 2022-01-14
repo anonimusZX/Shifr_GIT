2afeead30ee61fdc030bfcc53818fbc5b779d320
Volodymyr_Skrypnyk <82385420+anonimusZX@users.noreply.github.com>
2022-01-14 20:22:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from colorama import Back , init
+init()
+print(Back.BLUE + """
+________$$$$
+_______$$__$
+_______$___$$
+_______$___$$
+_______$$___$$
+________$____$$
+________$$____$$$
+_________$$_____$$
+_________$$______$$
+__________$_______$$
+____$$$$$$$________$$
+__$$$_______________$$$$$$
+_$$____$$$$____________$$$
+_$___$$$__$$$____________$$
+_$$________$$$____________$
+__$$____$$$$$$____________$
+__$$$$$$$____$$___________$
+__$$_______$$$$___________$
+___$$$$$$$$$__$$_________$$
+____$________$$$$_____$$$$
+____$$____$$$$$$____$$$$$$
+_____$$$$$$____$$__$$
+_______$_____$$$_$$$
+________$$$$$$$$$$
+""")
+start_wel = "Введіть слово:"
+wel_input = input(start_wel)
+input_list = [wel_input[0] ,wel_input[1] ,wel_input[2] ,wel_input[3] ,wel_input[4]]
+num_prog = input("Введіть номер програми (1-5):")
+if num_prog == "1":
+    first_prog = input_list[0] * 2 + input_list[1] * 2 + input_list[2] *  2 + input_list[3] * 2 + input_list[4] * 2    
+    print(first_prog)
+if num_prog == "2":
+    second_prog = input_list[1] + input_list[0] + input_list[3] + input_list[2] + input_list[4]
+    print(second_prog)
+if num_prog == "3":
+    third_prog = input_list[0] + input_list[4] + input_list[1] + input_list[4] + input_list[2] + input_list[4] + input_list[3] + input_list[4] + input_list[4]
+    print(third_prog)
+if num_prog == "4":
+    print("Поки не працює")
+if num_prog == "5":
+    print("Поки не працює")
